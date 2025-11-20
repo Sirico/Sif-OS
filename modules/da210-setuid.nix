e2d6f5d155b839e531edc4a67ecf219c8d9d6fd8
Darren Gregory <darrenjgregory@gmail.com>
2025-11-20 14:59:15 +0000

    DA210: drop setuid wrapper to fix suid-sgid-wrappers failure

--- modules/da210-setuid.nix
+++ modules/da210-setuid.nix
@@ -7,15 +7,6 @@ in
   options = { };
 
   config = {
-    # Install a setuid wrapper for thermalprinterut (lives outside the store).
-    security.wrappers.thermalprinterut = {
-      source = "${da210Driver}/share/tscbarcode/thermalprinterut";
-      owner = "root";
-      group = "root";
-      setuid = true;
-      permissions = "755";
-    };
-
     # Ship an explicit tmpfiles override so systemd ignores the store copy and
     # doesn't try to chmod it (avoids the read-only fchmod error you hit).
     environment.etc."tmpfiles.d/01-da210.conf".text = ''
